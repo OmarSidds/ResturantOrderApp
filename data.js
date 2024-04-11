1fd25ccd38038f40c7b9cc7725344e6024180c6e
OmarSidds <101479068+OmarSidds@users.noreply.github.com>
2024-04-11 14:05:14 -0400

    Added Header
    Added header with text on top of it

---
+++ data.js
@@ -0,0 +1,23 @@
+const menuArray = [
+    {
+        name: "Pizza",
+        ingredients: ["pepperoni", "mushrom", "mozarella"],
+        price: 14,
+        emoji: "🍕",
+        id: 0
+    },
+    {
+        name: "Hamburger",
+        ingredients: ["beef", "cheese", "lettuce"],
+        price: 12,
+        emoji: "🍔",
+        id: 1
+    },
+        {
+        name: "Soda",
+        ingredients: ["artificial flavor, water"],
+        price: 2,
+        emoji: "🥤",
+        id: 2
+    }
+]
